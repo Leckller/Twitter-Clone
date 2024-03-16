bf45ed9c0e127bd6303e44229117a0626b404460
Leckller <7leckler@gmail.com>
2024-03-16 00:39:27 -0300

    feat: setup

---
+++ src/database/config/database.ts
@@ -0,0 +1,12 @@
+import { Options } from "sequelize";
+
+const config: Options = {
+  database: process.env.ENV_DATABASE || 'localDB',
+  password: process.env.ENV_PASSWORD || 'password',
+  host: process.env.ENV_HOST || 'localhost',
+  username: process.env.ENV_USER || 'random',
+  port: Number(process.env.ENV_PORT) || 3101,
+  dialect: 'mysql'
+}
+
+export = config;
